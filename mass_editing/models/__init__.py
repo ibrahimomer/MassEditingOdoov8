25d2932556c442acfe00d32761c1cb255c6ac5a2
i.omar@nag.com.sa <i.omar@nag.com.sa>
2019-10-14 09:16:50 +0300

    upload mass editing odoo version 8 in odoo apps

---
+++ mass_editing/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import ir_model_fields
+import mass_object
